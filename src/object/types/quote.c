3c4b50807c1ccb55b622bbd23307b0a60b1f0a43
Jeff <jeffrey.a.meunier@gmail.com>
2025-12-10 04:41:36 -0600

    Added Quote type.

---
+++ src/object/types/quote.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include "_typedefs.h"
+
+#include "object/object.h"
+#include "object/functions/eval_rec.h"
+#include "object/functions/show.h"
+
+/* Defines *******************************************************************/
+
+/* Types *********************************************************************/
+
+struct Quote {
+    struct Object obj;
+    struct Object* expr;
+};
+
+/* Forward declarations ******************************************************/
+
+/* Global variables **********************************************************/
+
+/* Lifecycle functions *******************************************************/
+
+struct Quote* quote_new(struct Object* expr) {
+    struct Quote* quote = (struct Quote*)object_new(OT_Quote, NWORDS(*quote));
+    quote->expr = expr;
+    return quote;
+}
+
+/* Public functions **********************************************************/
+
+/* Unique functions ******************/
+
+/* Object functions ******************/
+
+bool_t quote_eval(struct Quote* quote, struct Etor_rec* etor, struct Object **value) {
+    *value = quote->expr;
+    return true;
+}
+
+void quote_show(struct Quote* quote, FILE* stream) {
+    fputc('\'', stream);
+    show(quote->expr, stream);
+    fputc('\'', stream);
+}
---
+++ src/object/types/quote.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdio.h>
+
+#include "_typedefs.h"
+
+#include "object/object.h"
+#include "object/functions/eval_rec.h"
+#include "object/functions/show.h"
+
+/* Defines *******************************************************************/
+
+/* Types *********************************************************************/
+
+struct Quote {
+    struct Object obj;
+    struct Object* expr;
+};
+
+/* Forward declarations ******************************************************/
+
+/* Global variables **********************************************************/
+
+/* Lifecycle functions *******************************************************/
+
+struct Quote* quote_new(struct Object* expr);
+
+/* Public functions **********************************************************/
+
+/* Unique functions ******************/
+
+/* Object functions ******************/
+
+bool_t quote_eval(struct Quote* quote, struct Etor_rec* etor, struct Object **value);
+void quote_show(struct Quote* quote, FILE* stream);
